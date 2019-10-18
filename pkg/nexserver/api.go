b62367cc113aa648f660dd02d94d29387b8f0105
Chan Shik Lim <chanshik@gmail.com>
2019-10-18 18:07:34 +0900

    Add get list all of agents and all of nodes /api/agents: all of agents /api/nodes: all of nodes

--- pkg/nexserver/api.go
+++ pkg/nexserver/api.go
@@ -26,6 +26,8 @@ func (s *NexServer) SetupApiHandler() {
 		clusters.GET("/:cid/agents", s.ApiAgentList)
 		clusters.GET("/:cid/nodes", s.ApiNodeList)
 	}
+	router.GET("/api/agents", s.ApiAgentListAll)
+	router.GET("/api/nodes", s.ApiNodeListAll)
 
 	go func() {
 		err := router.Run(fmt.Sprintf("%s:%d", s.config.Server.BindAddress, s.config.Server.ApiPort))
@@ -121,6 +123,56 @@ func (s *NexServer) ApiAgentList(c *gin.Context) {
 	})
 }
 
+func (s *NexServer) ApiAgentListAll(c *gin.Context) {
+	rows, err := s.db.Table("agents").
+		Select("agents.id, agents.version, agents.ipv4, agents.online, clusters.name").
+		Joins("left join clusters on agents.cluster_id=clusters.id").Rows()
+	if err != nil {
+		s.ApiResponseJson(c, 500, "bad",
+			fmt.Sprintf("failed to get data: %v", err))
+		return
+	}
+
+	type AgentItem struct {
+		Id      uint   `json:"id"`
+		Version string `json:"version"`
+		Ip      string `json:"ip"`
+		Online  bool   `json:"online"`
+	}
+	clusterMap := make(map[string][]*AgentItem)
+
+	var id uint
+	var version string
+	var ip string
+	var online bool
+	var clusterName string
+	for rows.Next() {
+		err := rows.Scan(&id, &version, &ip, &online, &clusterName)
+		if err != nil {
+			continue
+		}
+		_, found := clusterMap[clusterName]
+		if !found {
+			clusterMap[clusterName] = make([]*AgentItem, 0)
+		}
+
+		items := clusterMap[clusterName]
+		items = append(items, &AgentItem{
+			Id:      id,
+			Version: version,
+			Ip:      ip,
+			Online:  online,
+		})
+		clusterMap[clusterName] = items
+	}
+
+	c.JSON(200, gin.H{
+		"status":  "ok",
+		"message": "",
+		"data":    clusterMap,
+	})
+}
+
 func (s *NexServer) ApiNodeList(c *gin.Context) {
 	cID := c.Param("cid")
 	if cID == "" {
@@ -166,5 +218,67 @@ func (s *NexServer) ApiNodeList(c *gin.Context) {
 		"status":  "ok",
 		"message": "",
 		"data":    items,
+	})
+}
+
+func (s *NexServer) ApiNodeListAll(c *gin.Context) {
+	rows, err := s.db.Table("nodes").
+		Select("nodes.id, nodes.host, nodes.ipv4, nodes.os, nodes.platform, nodes.platform_family, nodes.platform_version, nodes.agent_id, clusters.name").
+		Joins("left join clusters on nodes.cluster_id=clusters.id").Rows()
+	if err != nil {
+		s.ApiResponseJson(c, 500, "bad",
+			fmt.Sprintf("failed to get data: %v", err))
+		return
+	}
+
+	type NodeItem struct {
+		Id              uint   `json:"id"`
+		Host            string `json:"host"`
+		Ip              string `json:"ip"`
+		Os              string `json:"os"`
+		Platform        string `json:"platform"`
+		PlatformFamily  string `json:"platform_family"`
+		PlatformVersion string `json:"platform_version"`
+		AgentId         uint   `json:"agent_id"`
+	}
+	clusterMap := make(map[string][]*NodeItem)
+
+	var id uint
+	var host string
+	var ip string
+	var os string
+	var platform string
+	var platformFamily string
+	var platformVersion string
+	var agentId uint
+	var clusterName string
+	for rows.Next() {
+		err := rows.Scan(&id, &host, &ip, &os, &platform, &platformFamily, &platformVersion, &agentId, &clusterName)
+		if err != nil {
+			continue
+		}
+		_, found := clusterMap[clusterName]
+		if !found {
+			clusterMap[clusterName] = make([]*NodeItem, 0)
+		}
+
+		items := clusterMap[clusterName]
+		items = append(items, &NodeItem{
+			Id:              id,
+			Host:            host,
+			Ip:              ip,
+			Os:              os,
+			Platform:        platform,
+			PlatformFamily:  platformFamily,
+			PlatformVersion: platformVersion,
+			AgentId:         agentId,
+		})
+		clusterMap[clusterName] = items
+	}
+
+	c.JSON(200, gin.H{
+		"status":  "ok",
+		"message": "",
+		"data":    clusterMap,
 	})
 }
